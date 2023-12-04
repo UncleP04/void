ecee0351fe6b1dd56f8f9138c1130c2c5d9032c7
Unclephil <philipnssien@gmail.com>
2023-12-05 00:57:45 +0100

    url

--- src/app/(pages)/Home/pages.jsx
+++ src/app/(pages)/Home/pages.jsx
@@ -24,7 +24,7 @@ const Landing = () => {
       <div className="w-[100%] h-[100%] fixed top-0 left-0 my-0 mx-auto flex justify-center items-center flex-col construction">
         <h1 className="text-white text-[2em] mb-4 text-center">Site is Currently under construction.</h1>
         <Image src={'/Image/construct.svg'} alt="Site under construction" width={700} height={300} className="lg:w-[60%] lg:h-[60%] w-[100%] p-6"/>
-        <Link href={'http://localhost:3000/void'} className="text-white bg-primary py-3 px-4 rounded-md">Enroll Now</Link>
+        <Link href={'/void'} className="text-white bg-primary py-3 px-4 rounded-md">Enroll Now</Link>
       </div>
    </div>
   )
