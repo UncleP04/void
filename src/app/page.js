5f06022adff1b1c955d464aa9c1e828f889e7519
PhilipDev <95418182+UnclePhil1@users.noreply.github.com>
2024-01-02 19:02:29 +0100

    Update the void page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import Landing from './(pages)/Home/pages'
+import Void from './(pages)/Void/page'
 
 export default function Home() {
   return (
    <div className='w-[100%] h-auto'>
-      <Landing />
+      <Void />
    </div>
   )
 }
